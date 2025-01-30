23321744c327e72a81625691a52d8d212602cfb1
Clément MM <clement.mollymitton@gmail.com>
2025-01-30 17:41:34 +0100

    Part 1, question 4
    I have add the proof of stake consensus with a json database to track
    the blance of each model. I have also implemented the slashing, reward
    and the network esclusion when the balance go under 0.

--- A/client.py
+++ A/client.py
@@ -1,5 +1,6 @@
 import requests
 import numpy as np
+import json
 
 BASED_URL = {
     "clement" : "http://localhost:5000",
@@ -9,15 +10,30 @@ BASED_URL = {
 
 model_weights = {model:1 for model in BASED_URL}
 predictions = []
+DB_FILE = "database.json"
+BASE_BALANCE = 1000
+
+def load_balances():
+    try:
+        with open(DB_FILE, "r") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {model: BASE_BALANCE for model in BASED_URL} 
+
+def save_balances(balances):
+    with open(DB_FILE, "w") as file:
+        json.dump(balances, file, indent=4)
+
+balances = load_balances()
 
 def consensus_prediction(data):
-    global predictions
+    global predictions, balances
     predictions = []
 
     # Get the prediction
     for model_name, url in BASED_URL.items():
         response = requests.post(url + "/predict", json=data)
-        if response.status_code == 200:
+        if response.status_code == 200 and balances[model_name]>0:
             predictions.append((model_name, response.json().get("prediction")))
 
     if not predictions:
@@ -28,24 +44,30 @@ def consensus_prediction(data):
     for model_name, prediction in predictions:
         consensus += np.array(prediction) * model_weights[model_name]
 
-    update_weight(np.argmax(consensus))
     return np.argmax(consensus)
 
 
-def update_weight(real_prediction, alpha=0.1):
-    global model_weights
+def update(real_prediction, alpha=0.1, penalty=50, reward=10):
+    global model_weights, balances
 
     for model_name, prediction in predictions:
         predicted_class = np.argmax(prediction)
 
         if predicted_class == real_prediction:
             model_weights[model_name] *= (1 + alpha)
+            balances[model_name] += reward
         else:
             model_weights[model_name] *= (1 - alpha)
+            balances[model_name] -= penalty
+            if balances[model_name] <= 0:
+                print(f"{model_name} is excluded of the network because it doesn't have sufficient fund.")
+
 
     total_weight = sum(model_weights.values())
     if total_weight > 0:
         model_weights = {model: weight / total_weight for model, weight in model_weights.items()}
+    
+    save_balances(balances)
 
 
 if __name__ == '__main__':
@@ -55,5 +77,8 @@ if __name__ == '__main__':
     }
 
     predicted_class = consensus_prediction(data)
+    if predicted_class is not None:
+        update(predicted_class)
     print(f"Prediction of the consensus : {predicted_class}")
     print(f"New weights : {model_weights}")
+    print(f"New balance : {balances}")
